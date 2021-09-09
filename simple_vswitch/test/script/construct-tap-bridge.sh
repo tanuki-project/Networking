c76ac1dc0d4adf733487ffd62d84a98d413b9ec5
Takahiro Sayama <cleverraccoon@gmail.com>
2021-09-09 17:40:28 +0900

    rename interface and namespace

--- simple_vswitch/test/script/construct-tap-bridge.sh
+++ simple_vswitch/test/script/construct-tap-bridge.sh
@@ -1,49 +1,57 @@
 #!/bin/sh
 
-ip link add veth0-in type veth peer name veth0-out
-ip link add veth1-in type veth peer name veth1-out
-ip link add veth2-in type veth peer name veth2-out
-ip link add veth3-in type veth peer name veth3-out
+ip link add veth1-vsw type veth peer name veth1
+ip link add veth2-vsw type veth peer name veth2
+ip link add veth3-vsw type veth peer name veth3
+ip link add veth4-vsw type veth peer name veth4
 
-ip netns add veth0
-ip netns add veth1
-ip netns add veth2
-ip netns add veth3
+ip netns add NS1
+ip netns add NS2
+ip netns add NS3
+ip netns add NS4
 
-ip link set veth0-out netns veth0
-ip link set veth1-out netns veth1
-ip link set veth2-out netns veth2
-ip link set veth3-out netns veth3
+ip link set veth1 netns NS1
+ip link set veth2 netns NS2
+ip link set veth3 netns NS3
+ip link set veth4 netns NS4
 
-ip link set veth0-in up
-ip netns exec veth0 ip link set veth0-out up
-ip netns exec veth0 ip address add 192.168.100.101/24 dev veth0-out
+ip link set veth1-vsw up
+ip netns exec NS1 ip link set veth1 up
+ip netns exec NS1 ip address add 192.168.100.101/24 dev veth1
+ip netns exec NS1 ip link set lo up
+ip netns exec NS1 ip address add 127.0.0.1/24 dev lo
 
-ip link set veth1-in up
-ip netns exec veth1 ip link set veth1-out up
-ip netns exec veth1 ip address add 192.168.100.102/24 dev veth1-out
+ip link set veth2-vsw up
+ip netns exec NS2 ip link set veth2 up
+ip netns exec NS2 ip address add 192.168.100.102/24 dev veth2
+ip netns exec NS2 ip link set lo up
+ip netns exec NS2 ip address add 127.0.0.1/24 dev lo
 
-ip link set veth2-in up
-ip netns exec veth2 ip link set veth2-out up
-ip netns exec veth2 ip address add 192.168.100.103/24 dev veth2-out
+ip link set veth3-vsw up
+ip netns exec NS3 ip link set veth3 up
+ip netns exec NS3 ip address add 192.168.100.103/24 dev veth3
+ip netns exec NS3 ip link set lo up
+ip netns exec NS3 ip address add 127.0.0.1/24 dev lo
 
-ip link set veth3-in up
-ip netns exec veth3 ip link set veth3-out up
-ip netns exec veth3 ip address add 192.168.100.104/24 dev veth3-out
+ip link set veth4-vsw up
+ip netns exec NS4 ip link set veth4 up
+ip netns exec NS4 ip address add 192.168.100.104/24 dev veth4
+ip netns exec NS4 ip link set lo up
+ip netns exec NS4 ip address add 127.0.0.1/24 dev lo
 
-ip addr show veth0-in
-ip netns exec veth0 ip addr show veth0-out
-ip addr show veth1-in
-ip netns exec veth1 ip addr show veth1-out
-ip addr show veth2-in
-ip netns exec veth2 ip addr show veth2-out
-ip addr show veth3-in
-ip netns exec veth3 ip addr show veth3-out
+ip addr show veth1-vsw
+ip netns exec NS1 ip addr show veth1
+ip addr show veth2-vsw
+ip netns exec NS2 ip addr show veth2
+ip addr show veth3-vsw
+ip netns exec NS3 ip addr show veth3
+ip addr show veth4-vsw
+ip netns exec NS4 ip addr show veth4
 
-../../bin/cmd/vswconfig add veth0-in > /dev/null
-../../bin/cmd/vswconfig add veth1-in > /dev/null
-../../bin/cmd/vswconfig add veth2-in > /dev/null
-../../bin/cmd/vswconfig add veth3-in > /dev/null
+../../bin/cmd/vswconfig add veth1-vsw > /dev/null
+../../bin/cmd/vswconfig add veth2-vsw > /dev/null
+../../bin/cmd/vswconfig add veth3-vsw > /dev/null
+../../bin/cmd/vswconfig add veth4-vsw > /dev/null
 
 exit 0
 
--- simple_vswitch/test/script/destruct-tap-bridge.sh
+++ simple_vswitch/test/script/destruct-tap-bridge.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-../../bin/cmd/vswconfig delete veth0-in > /dev/null
-../../bin/cmd/vswconfig delete veth1-in > /dev/null
-../../bin/cmd/vswconfig delete veth2-in > /dev/null
-../../bin/cmd/vswconfig delete veth3-in > /dev/null
+../../bin/cmd/vswconfig delete veth1-vsw > /dev/null
+../../bin/cmd/vswconfig delete veth2-vsw > /dev/null
+../../bin/cmd/vswconfig delete veth3-vsw > /dev/null
+../../bin/cmd/vswconfig delete veth4-vsw > /dev/null
 
-ip netns delete veth0
-ip netns delete veth1
-ip netns delete veth2
-ip netns delete veth3
+ip netns delete NS1
+ip netns delete NS2
+ip netns delete NS3
+ip netns delete NS4
 
 exit 0
 
